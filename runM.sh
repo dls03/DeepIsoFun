370fff5c87afedb846e86cb3aff9ab1cca075116
dls03 <40154097+dls03@users.noreply.github.com>
2018-06-24 23:35:13 -0700

    Update runM.sh

--- runM.sh
+++ runM.sh
@@ -22,7 +22,7 @@ module load glog
 
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/rhome/dshaw003/bigdata/caffe/openblas/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:Path_to_openblas
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/linux/centos/7.x/x86_64/pkgs/gflags/2.1.2/lib/
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/linux/centos/7.x/x86_64/pkgs/leveldb/1.20/out-shared
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/linux/centos/7.x/x86_64/pkgs/glog/0.3.5/lib:/opt/linux/centos/7.x/x86_64/pkgs/cuDNN/6.0/lib64:/opt/linux/centos/7.x/x86_64/pkgs/cuda/8.0/lib64:/opt/linux/centos/7.x/x86_64/pkgs/cuda/8.0/nvvm/lib64:/opt/linux/centos/7.x/x86_64/pkgs/cuda/7.0/lib64:/opt/linux/centos/7.x/x86_64/pkgs/cuda/7.0/nvvm/lib64:/opt/linux/centos/7.x/x86_64/pkgs/cuda/7.0/lib:/opt/linux/centos/7.x/x86_64/pkgs/python/2.7.5/lib:/opt/linux/centos/7.x/x86_64/pkgs/ggobi/2.1.11/lib:/opt/linux/centos/7.x/x86_64/pkgs/R/3.4.0/lib64/R/lib:/opt/linux/centos/7.x/x86_64/pkgs/openmpi/2.0.1-slurm-16.05.4/lib:/opt/linux/centos/7.x/x86_64/pkgs/openmpi/2.0.1-slurm-16.05.4/lib/openmpi:/opt/linux/centos/7.x/x86_64/pkgs/slurm/16.05.4/lib:/rhome/dshaw003/bigdata/caffe/openblas/lib
